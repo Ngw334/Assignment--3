fc142d99eb7088863269f2208bee66df353b068e
Nishant Wankhade <82573863+Ngw334@users.noreply.github.com>
2021-07-17 13:30:09 +0530

    Update assignment3Q5.cpp

--- assignment3Q5.cpp
+++ assignment3Q5.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n)
+{
+    for(int i = 2; i < n; i++)
+    {
+        if( n % i== 0)
+        {
+            return false;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return true;
+}
 
 int main()
 {
   for(int n = 4; n < 100; n++){   // Since to print prime no between 3 and 100 n must be 4;
-    for(int c = 2; c < n ; c++)
+    if(isPrime(n))
     {
-      if(n % c == 0)
-      {
-        break;
-      }
-      else
-      {
         cout<< n << endl;
-        break;
-      }
+        continue;
+    }
+    else
+    {
+        continue;
     }
   }
 }
- 
